3a2114026f4662dc47e5cfcae0b551382fe70de2
Sophie Carneiro Esteves <sophie.carneiro-esteves@cpe.fr>
2023-11-08 09:40:44 +0100

    lol

--- code/test_LA.py
+++ code/test_LA.py
@@ -31,7 +31,7 @@ def test_calculate_metric(epoch_num):
     net.eval()
 
     avg_metric = test_all_case(net, image_list, num_classes=num_classes,
-                               patch_size=(112, 112, 80), stride_xy=18, stride_z=4,
+                               patch_size=(96, 96, 96), stride_xy=48, stride_z=48,
                                save_result=True, test_save_path=test_save_path)
 
     return avg_metric
